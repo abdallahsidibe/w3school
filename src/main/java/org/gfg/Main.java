7343e9c937b147cc19de5549c11397c2df6538fd
abdallahsidibe <sidibeabdallah01@gmail.com>
2023-12-01 22:18:38 +0100

    first commit

---
+++ src/main/java/org/gfg/Main.java
@@ -0,0 +1,31 @@
+package org.gfg;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Entrée with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Maj+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+            int carName;
+            int car_name;
+            String camelCaseInput1 = "myNiceCamelCaseString";
+            String camelCaseInput2 = "MyNiceCamelCaseString";
+            String expectedInSnakeCase = "my_nice_camel_case_string";
+
+            // public class GroupCar{
+            // addCar();
+            //deletePerson()
+            // }
+
+            // formater son code: ctrl + alt + L
+        }
+    }
+}
